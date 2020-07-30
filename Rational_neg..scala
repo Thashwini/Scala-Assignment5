3e4c905dfc42e0cbe60e881716b05df7f908426c
Thashwini <56554336+Thashwini@users.noreply.github.com>
2020-07-30 10:30:15 +0530

    Q1 Edited

---
+++ Rational_neg..scala
@@ -0,0 +1,22 @@
+object Neg extends App{
+  val r=new Rational_neg(2,4)
+
+  val x=r.neg
+  print("Entered rational number: ")
+  println(r)
+  
+  print("Negative value of the entered rational number: ")
+  println(x)
+}
+
+class Rational_neg(n:Int,d:Int){
+
+  def numer=n/gcd(n,d)
+  def denom=d/gcd(n,d)
+  private def gcd(a:Int,b:Int):Int=if(b==0) a else gcd(b,a%b)
+
+  def neg = new Rational_neg(-this.numer,this.denom)
+
+
+  override def toString=numer+"/"+denom
+}
